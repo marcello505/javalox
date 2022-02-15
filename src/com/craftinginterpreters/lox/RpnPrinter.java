9595f46c8a926be79a0364940768900815f66ee4
Marcello Haddeman <marcellohaddeman@gmail.com>
2022-02-15 22:53:00 +0100

    Finished Ch5_Challenge3

---
+++ src/com/craftinginterpreters/lox/RpnPrinter.java
@@ -0,0 +1,62 @@
+package com.craftinginterpreters.lox;
+
+public class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr){
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return rpn(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return rpn("", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return rpn(expr.operator.lexeme, expr.right);
+    }
+
+    private String rpn(String name, Expr... exprs){
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < exprs.length; i++){
+            if(i != 0){
+                builder.append(" ");
+            }
+            builder.append(exprs[i].accept(this));
+        }
+
+        if(!name.equals("")){
+            builder.append(" ");
+            builder.append(name);
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args){
+        Expr expression = new Expr.Binary(
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(1),
+                                new Token(TokenType.PLUS, "+", null, 1),
+                                new Expr.Literal(2))),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(4),
+                                new Token(TokenType.MINUS, "-", null, 1),
+                                new Expr.Literal(3))));
+        System.out.println(new RpnPrinter().print(expression));
+    }
+}
